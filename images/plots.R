ebee5399bea2085b2910cec4818bb88a19f98927
Tyler Forrester <tyler.forrester@gmail.com>
2019-05-06 12:39:31 -0600

    added plots.R

--- images/plots.R
+++ images/plots.R
@@ -3,7 +3,7 @@ nslr2 = read.csv("file:///C:/Users/Tyler/coveragestudymentchandhooker/estimates_
 slr = slr[order(slr$Estimate), ]
 nslr2$TCI = nslr2$Estimate  +  2*nslr2$SE
 nslr2$LCI = nslr2$Estimate  -  2*nslr2$SE
-hist(slr$Estimate, xlab="Point Estimate Theta=33.4", main="Coverage Study NSLR2")
+hist(slr$Estimate, xlab="Point Estimate Theta=20",main="SLR")
 hist(4*slr$SE, xlab="2-Sigma CI Width", main="Coverage Study NSLR2")
 hist(6*slr$SE, xlab="3-Sigma CI Width", main="Coverage Study NSLR2")
 
@@ -15,6 +15,6 @@ hist(slr$LCI)
 
 
 
-hist(nslr2$Estimate, xlab="Point Estimate Theta=33.4", main="Coverage Study NSLR2")
+hist(nslr2$Estimate, xlab="Point Estimate Theta=33.4",main="SNLR2")
 hist(4*nslr2$SE, xlab="2-Sigma CI Width", main="Coverage Study NSLR2")
 hist(6*nslr2$SE, xlab="3-Sigma CI Width", main="Coverage Study NSLR2")
---
+++ images/temp.R
@@ -0,0 +1,2 @@
+
+b_simple[k.small$cluster==1,]$Cluster = "Weak_Polar"
